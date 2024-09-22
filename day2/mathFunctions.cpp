e506223b73c1afa11b3211997d3c1cfbbf91161b
Alvin Joby <alvinjobyindia@gmail.com>
2024-09-22 21:35:00 +0530

    day2-updated

--- day2/mathFunctions.cpp
+++ day2/mathFunctions.cpp
@@ -13,15 +13,15 @@ int main() {
 
     z = std::pow(x, y); // returns x raised to the power of y
 
-    z = std::sqrt(x); // returns the square root of x
+    z = sqrt(x); // returns the square root of x
 
-    z = std::abs(x); // returns the absolute value of x
+    z = abs(x); // returns the absolute value of x
 
-    z = std::round(x); // returns the rounded value of x
+    z = round(x); // returns the rounded value of x
 
-    z = std::floor(x); // returns the largest integer less than or equal to x
+    z = floor(x); // returns the largest integer less than or equal to x
 
-    z = std::ceil(x); // returns the smallest integer greater than or equal to x
+    z = ceil(x); // returns the smallest integer greater than or equal to x
 
 
     std::cout << z << std::endl;
